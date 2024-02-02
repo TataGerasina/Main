51e83839c612113d55fd3006ecdfdea76f949c22
Tatyana Gerasina <gtstata@gmail.com>
2024-02-02 19:17:01 +0300

    Homework made

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public FormDate birthday; // поле для даты рождения
+    public String name; // поле для имени
+    public String passport; //  поле для паспортных данных
+    public String patronymic; //     поле для отчества
+    public String phone; //   поле для телефона
+    public String surname; //     поле для фамилии
+    public String subscription;// поле согласия с информированием о скидках в виде sms
+
+}
